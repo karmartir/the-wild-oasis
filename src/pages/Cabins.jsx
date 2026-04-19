243e817903fe60fcf81f12c08a9602cf28c4d424
karmartir <karmartir@gmail.com>
2026-04-19 12:33:33 -0400

    352. Introducing Another Library: React Hook Form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,12 @@
 import Heading from "../ui/Heading";
+import { useState } from "react";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -12,6 +16,12 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          {!showForm ? "Add new cabin" : "Close adding new cabin form"}
+        </Button>
+
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
