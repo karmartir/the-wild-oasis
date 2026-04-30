8e32e3974dac63843eb384ba245474d8d35cf9a1
karmartir <karmartir@gmail.com>
2026-04-29 23:00:42 -0400

    379. API-Side Filtering: Filtering Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+  //Filter
+  const filterValue = searchParams.get("status") || "all";
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue, method: "eq" };
+  // :{ field: "totalPrice", value: 5000, method: "gte" };
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => {
+      return getBookings({ filter });
+    }, //getBookings,
   });
   return { isLoading, error, bookings };
 }
