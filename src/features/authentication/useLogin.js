f1fe0dd86b90f03b2c8bf75ea7c372bd2bd0e07f
karmartir <karmartir@gmail.com>
2026-05-03 00:24:38 -0400

    390. Authorization: Protecting Routes

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,13 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (data) => {
+      queryClient.setQueryData(["user"], data.user);
       toast.success(`Welcome back ${data.user.email}`, {
         position: "top-right",
       });
