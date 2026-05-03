a487eeadce8d70d6b7e66faf34ec6ab6b19fb52b
karmartir <karmartir@gmail.com>
2026-05-03 14:44:51 -0400

    394. User Sign Up

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,80 +3,103 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSignup } from "./useSignup";
+import Spinner from "../../ui/Spinner";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, handleSubmit, getValues, formState } = useForm();
+  const { signup, isLoading } = useSignup();
+  const { register, handleSubmit, getValues, formState, reset } = useForm();
   const { errors } = formState;
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = ({ fullName, email, password }) => {
+    signup(
+      { fullName, email, password },
+      {
+        onSettled: () => {
+          reset();
+        },
+      },
+    );
   };
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow label="Full name" error={errors?.fullName?.message}>
-        <Input
-          type="text"
-          id="fullName"
-          {...register("fullName", { required: "This field is required" })}
-        />
-      </FormRow>
+    <>
+      {!isLoading ? (
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <FormRow label="Full name" error={errors?.fullName?.message}>
+            <Input
+              type="text"
+              id="fullName"
+              disabled={isLoading}
+              {...register("fullName", { required: "This field is required" })}
+            />
+          </FormRow>
 
-      <FormRow label="Email address" error={errors?.email?.message}>
-        <Input
-          type="email"
-          id="email"
-          {...register(
-            "email",
-            { required: "This field is required" },
-            {
-              pattern: {
-                value: /\S+@\S+\.\S+/,
-                message: "Provided value is not an email address",
-              },
-            },
-          )}
-        />
-      </FormRow>
+          <FormRow label="Email address" error={errors?.email?.message}>
+            <Input
+              type="email"
+              disabled={isLoading}
+              id="email"
+              {...register(
+                "email",
+                { required: "This field is required" },
+                {
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: "Provided value is not an email address",
+                  },
+                },
+              )}
+            />
+          </FormRow>
 
-      <FormRow
-        label="Password (min 8 characters)"
-        error={errors?.password?.message}
-      >
-        <Input
-          type="password"
-          id="password"
-          {...register("password", {
-            required: "This field is required",
-            minLength: {
-              value: 8,
-              message: "Password needs a minimum of 8 characters",
-            },
-          })}
-        />
-      </FormRow>
+          <FormRow
+            label="Password (min 8 characters)"
+            error={errors?.password?.message}
+          >
+            <Input
+              type="password"
+              id="password"
+              disabled={isLoading}
+              {...register("password", {
+                required: "This field is required",
+                minLength: {
+                  value: 8,
+                  message: "Password needs a minimum of 8 characters",
+                },
+              })}
+            />
+          </FormRow>
 
-      <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
-        <Input
-          type="password"
-          id="passwordConfirm"
-          {...register("passwordConfirm", {
-            required: "This field is required",
-            validate: (value) =>
-              value === getValues().password || "Passwords need to match",
-          })}
-        />
-      </FormRow>
+          <FormRow
+            label="Repeat password"
+            error={errors?.passwordConfirm?.message}
+          >
+            <Input
+              type="password"
+              disabled={isLoading}
+              id="passwordConfirm"
+              {...register("passwordConfirm", {
+                required: "This field is required",
+                validate: (value) =>
+                  value === getValues().password || "Passwords need to match",
+              })}
+            />
+          </FormRow>
 
-      <FormRow>
-        {/* type is an HTML attribute! */}
-        <Button>Create new user</Button>
-        <Button variation="secondary" type="reset">
-          Cancel
-        </Button>
-      </FormRow>
-    </Form>
+          <FormRow>
+            {/* type is an HTML attribute! */}
+            <Button disabled={isLoading}>Create new user</Button>
+            <Button disabled={isLoading} variation="secondary" type="reset">
+              Cancel
+            </Button>
+          </FormRow>
+        </Form>
+      ) : (
+        <Spinner />
+      )}
+    </>
   );
 }
 
