81ad49ec86f80bc86ff7db89aae81deb06171562
karmartir <karmartir@gmail.com>
2026-05-02 21:27:09 -0400

    389. Authentication: User Login With Supabase

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+  const navigate = useNavigate();
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (data) => {
+      toast.success(`Welcome back ${data.user.email}`, {
+        position: "top-right",
+      });
+      navigate("/dashboard");
+    },
+    onError: (err) => {
+      console.log("Error", err);
+      toast.error("Provided email or password is incorrect");
+    },
+  });
+  return { login, isLoading };
+}
