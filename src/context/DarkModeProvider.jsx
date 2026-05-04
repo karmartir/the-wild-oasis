e19df6230cf702e526bd98807e5143a089239b06
karmartir <karmartir@gmail.com>
2026-05-04 14:27:42 -0400

    398. Implementing Dark Mode With CSS Variables

---
+++ src/context/DarkModeProvider.jsx
@@ -0,0 +1,28 @@
+import { DarkModeContext } from "./DarkModeContext";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+import { useEffect } from "react";
+
+export function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+  return (
+    <DarkModeContext.Provider
+      value={{ isDarkMode, setIsDarkMode, toggleDarkMode }}
+    >
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
