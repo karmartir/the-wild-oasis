90fba89d9147d0978bc90e2de362abdc13a4eeef
karmartir <karmartir@gmail.com>
2026-05-05 22:22:44 -0400

    Fix Toggle click propagation and update List ref handling

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -84,6 +84,7 @@ export default function Menus({ children }) {
 function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext);
   const handleClick = (e) => {
+    e.stopPropagation();
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.right,
@@ -101,7 +102,7 @@ function Toggle({ id }) {
 
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
   if (openId !== id) return null;
   return createPortal(
     <StyledList position={position} ref={ref}>
